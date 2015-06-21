8040eb259fa7de935aa821553b780f3f1a1ac24c
Claudiu Soroiu <csoroiu@gmail.com>
2015-06-21 17:07:22 +0300

    did some fixes

--- src/main/java/ro/sixi/eval/common/ReversibleRandom.java
+++ src/main/java/ro/sixi/eval/common/ReversibleRandom.java
@@ -4,23 +4,23 @@ import java.lang.reflect.Field;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
-//https://github.com/votadlos/JavaCG/blob/master/JavaCG/JavaCG/JavaLCGMimic.cpp#L15
-public class ReversibleRandom extends Random {
+public final class ReversibleRandom extends Random {
     private static final long serialVersionUID = 5566331316770172904L;
 
     private final static long multiplier = 0x5DEECE66DL;
     private final static long invmultiplier = 0xDFE05BCB1365L;
     private final static long addend = 0xBL;
     private final static long mask = (1L << 48) - 1;
+    private final static double DOUBLE_UNIT = 0x1.0p-53; // 1.0 / (1L << 53)
 
     private AtomicLong seedRef;
 
     public ReversibleRandom(long seed) {
         super(seed);
-        getReferenceToSeed();
+        setReferenceToSeed();
     }
 
-    private void getReferenceToSeed() {
+    private void setReferenceToSeed() {
         try {
             Field f = Random.class.getDeclaredField("seed");
             f.setAccessible(true);
@@ -29,19 +29,26 @@ public class ReversibleRandom extends Random {
         }
     }
 
-    public ReversibleRandom() {
-        super();
-        getReferenceToSeed();
+    public long getSeed() {
+        return (seedRef.get() ^ multiplier) & mask;
     }
 
+    public ReversibleRandom() {
+        super();
+        setReferenceToSeed();
+    }
+
+    // https://github.com/votadlos/JavaCG/blob/master/JavaCG/JavaCG/JavaLCGMimic.cpp#L15
     protected int prev(int bits) {
-        long nextSeed, prevseed;
+        long nextSeed, prevSeed;
         AtomicLong seed = this.seedRef;
         do {
             nextSeed = seed.get();
-            prevseed = (invmultiplier * (nextSeed - addend)) & mask;
-        } while (!seed.compareAndSet(nextSeed, prevseed));
-        return (int) (prevseed >>> (48 - bits));
+            prevSeed = (invmultiplier * (nextSeed - addend)) & mask;
+        } while (!seed.compareAndSet(nextSeed, prevSeed));
+        // we generate the bits from the current seed,
+        // but we have updated the value of the seed with the prev one
+        return (int) (nextSeed >>> (48 - bits));
     }
 
     public int prevInt(int bound) {
@@ -49,7 +56,7 @@ public class ReversibleRandom extends Random {
             return bound;
 
         if ((bound & -bound) == bound) // limit is power of 2
-            return bound * prev(31) >> 31; // output limit*(seed >> 17) >> 31
+            return (int) (((long) bound * prev(31)) >> 31); // output limit*(seed >> 17) >> 31
 
         int j, k;
         do {// limit is not power of 2
@@ -61,6 +68,18 @@ public class ReversibleRandom extends Random {
 
     public int prevInt() {
         return prev(32);
+    }
+
+    public long prevLong() {
+        return prev(32) + ((long) (prev(32)) << 32);
+    }
+
+    public float prevFloat() {
+        return prev(24) / ((float) (1 << 24));
+    }
+
+    public double prevDouble() {
+        return (prev(27) + ((long) (prev(26)) << 27)) * DOUBLE_UNIT;
     }
 
     public boolean prevBoolean() {
