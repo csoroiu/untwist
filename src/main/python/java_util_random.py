7f68e666005690790331a5318dc8d832e247208a
Claudiu Soroiu <csoroiu@gmail.com>
2015-09-27 11:47:40 +0300

    cosmetic

--- src/main/python/java_util_random.py
+++ src/main/python/java_util_random.py
@@ -32,7 +32,7 @@ class JavaRandom(object):
     """
 
 
-    __seed_uniquifier = 8682522807148012;
+    __seed_uniquifier = 8682522807148012
 
 
     @staticmethod
@@ -77,18 +77,13 @@ class JavaRandom(object):
         """
         self.__seed = (seed ^ 0x5deece66d) & 0xFFFFFFFFFFFF  # ((1 << 48) - 1)
 
-    def next(self, bits):
+    def __next(self, bits):
         """
         Generate the next random number.
         As in Java, the general rule is that this method returns an int that
         is `bits` bits long, where each bit is nearly equally likely to be 0
         or 1.
         """
-
-        if bits < 1:
-            bits = 1
-        elif bits > 32:
-            bits = 32
 
         self.__seed = (self.__seed * 0x5deece66d + 0xb) & 0xFFFFFFFFFFFF  # ((1 << 48) - 1)
         retval = self.__seed >> (48 - bits)
@@ -117,7 +112,7 @@ class JavaRandom(object):
         """
 
         if n is None:
-            return self.next(32)
+            return self.__next(32)
 
         if n <= 0 or n > 0x7FFFFFFF:
             raise ValueError("Argument must be 32 bit signed integer with positive value!")
@@ -130,12 +125,12 @@ class JavaRandom(object):
 
         # Is this a power of two?
         if not (n & (n - 1)):
-            return (n * self.next(31)) >> 31
+            return (n * self.__next(31)) >> 31
 
-        bits = self.next(31)
+        bits = self.__next(31)
         val = bits % n
         while _signed_int(bits - val + (n - 1)) < 0:
-            bits = self.next(31)
+            bits = self.__next(31)
             val = bits % n
 
         return val
@@ -147,14 +142,14 @@ class JavaRandom(object):
         so we generate two 32-bit numbers and glue them together.
         """
 
-        return (self.next(32) << 32) + self.next(32)
+        return (self.__next(32) << 32) + self.__next(32)
 
     def next_boolean(self):
         """
         Return a random bool.
         """
 
-        return bool(self.next(1))
+        return bool(self.__next(1))
 
     def next_float(self):
         """
@@ -164,11 +159,11 @@ class JavaRandom(object):
         precision space.
         """
 
-        return self.next(24) / float(1 << 24)
+        return self.__next(24) / float(1 << 24)
 
     def next_double(self):
         """
         Return a random float in (0, 1).
         """
 
-        return ((self.next(26) << 27) + self.next(27)) / float(1 << 53)
+        return ((self.__next(26) << 27) + self.__next(27)) / float(1 << 53)
