5d4655bdcb40503caee8af46baf4559eb616463f
Claudiu Soroiu <csoroiu@gmail.com>
2015-12-03 01:31:43 +0200

    moved python module to correct package

---
+++ src/main/python/eval/util/test_generator_utils.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+def get_permutation(n, randomizer, steps=1):
+    perm = []
+    for i in range(n):
+        perm.append(i)
+    for _ in range(steps):
+        # http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+        for i in range(n):
+            j = randomizer.next_int(n)
+            if i == j:
+                continue
+            perm[i] ^= perm[j]
+            perm[j] ^= perm[i]
+            perm[i] ^= perm[j]
+    return perm
+
+
+def get_permutation_inside_out(n, randomizer):
+    # http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    perm = [0]
+    for i in range(1, n):
+        j = randomizer.next_int(i + 1)
+        perm.append(perm[j])
+        perm[j] = i
+    return perm
