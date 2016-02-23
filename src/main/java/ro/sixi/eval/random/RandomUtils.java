cc2e9edd7df059cc9ee1a66c44b2802e38e9563c
Claudiu Soroiu <csoroiu@gmail.com>
2016-02-23 22:05:58 +0200

    added a helper method

--- src/main/java/ro/sixi/eval/random/RandomUtils.java
+++ src/main/java/ro/sixi/eval/random/RandomUtils.java
@@ -7,6 +7,11 @@ class RandomUtils {
         return RandomGeneratorFactory.convertToLong(seed);
     }
 
+    static int convertToInt(int high, int low) {
+        final int prime = 65521;
+        return high * prime + low;
+    }
+
     static int convertToInt(int... seed) {
         // The following number is the largest prime that fits
         // in 16 bits (i.e. 2^32 - 5).
